17ad04815bd4def661ec933554c84780d737290d
orpqaab <orpqaab@gmail.com>
2016-12-28 13:59:30 +0100

    Formatting and rename parameter

--- Application.php
+++ Application.php
@@ -1,14 +1,18 @@
 <?php
+
 namespace Bevprog\CrudDemo;
 
 class Application {
-    private $controllers = [
-            "create"=>  "Bevprog\CrudDemo\Controllers\CreateController" 
-        ];
-    public function run($request) {
-        $controller=$this->controllers[$request["controller"]];
-        $action=$request["action"];
-        $controllerInstance= new $controller();
+
+    private $controllers = ["create" => "Bevprog\CrudDemo\Controllers\CreateController"];
+
+    public function run($query_params) {
+
+        $controller = $this->controllers[$query_params["controller"]];
+        $action = $query_params["action"];
+
+        $controllerInstance = new $controller();
         $controllerInstance->$action();
     }
+
 }
