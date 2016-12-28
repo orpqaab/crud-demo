d72802c034f825665adc939c41457f89f326b00d
orpqaab <orpqaab@gmail.com>
2016-12-28 13:59:09 +0100

    Refactor global constants into Configuration object

---
+++ Configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bevprog\CrudDemo;
+
+class Configuration {
+    const ROOT_DIR        = __DIR__;
+    const CONTROLLERS_DIR = self::ROOT_DIR."/controllers";
+    const VIEWS_DIR       = self::ROOT_DIR."/views";
+
+}
+
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-define("ROOT_DIR", __DIR__);
-define("CONTROLLERS_DIR", ROOT_DIR."/controllers");
-define("VIEWS_DIR",ROOT_DIR."/views");
 
 require 'autoload.php';
 
