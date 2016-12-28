dff4e3c6a3aa0f9da4cc19efaf9eb2eaf7efa9de
orpqaab <orpqaab@gmail.com>
2016-12-28 15:55:08 +0100

    simple db_query listing and templating

--- controllers/ListController.php
+++ controllers/ListController.php
@@ -5,8 +5,17 @@ namespace Bevprog\CrudDemo\Controllers;
 class ListController {
     
     public function listItems() {
+        $dbh= new \PDO('mysql:dbname=employees;host=localhost','root','');
+        $stmt= $dbh->prepare("SELECT * FROM employees limit 10");
+        $stmt->execute();
+        $result= $stmt->fetchAll();
         
-        $template = \Bevprog\CrudDemo\Configuration::VIEWS_DIR."/list_items.html";
-        echo implode("",file($template));
+        $employees = $result;
+        ob_start();
+        include \Bevprog\CrudDemo\Configuration::VIEWS_DIR."/list_items.html";
+        $output = ob_get_flush();
+        
+        echo $output;
+        
     }
 }
